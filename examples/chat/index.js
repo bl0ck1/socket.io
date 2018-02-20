13d244c8555acb5a98b3b904ca22bb4aa068bc39
bl0ck1 <36583269+bl0ck1@users.noreply.github.com>
2018-02-20 12:16:05 -0600

    Update index.js

--- examples/chat/index.js
+++ examples/chat/index.js
@@ -21,7 +21,7 @@ io.on('connection', function (socket) {
   var addedUser = false;
 
   // when the client emits 'new message', this listens and executes
-  socket.on('new message', function (data) {
+  socket.on('broad', function (data) {
     // we tell the client to execute 'new message'
     socket.broadcast.emit('new message', {
       username: socket.username,
